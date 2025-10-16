652486d627eb5aabe2af36804181ffb5e1ed6f15
Harsh530511 <harshking5305@gmail.com>
2025-10-16 09:21:25 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -11,7 +11,7 @@ const io = new Server(httpServer, {
   cors: {
     origin: [
       "https://chesss-production.up.railway.app", // your Railway URL
-      "http://localhost:3000", // local testing
+      "http://localhost:8080", // local testing
       "http://127.0.0.1:5000"
     ],
     methods: ["GET", "POST"]
